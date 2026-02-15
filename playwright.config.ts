df9c810d8467177bdde135fe2d3bda360afb1962
Iván Sánchez <11472842+isanchezd@users.noreply.github.com>
2026-02-15 12:55:55 +0100

    chore: Fix load test e2e and add test-results to .gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'npm run dev',
+		port: 5173,
+		reuseExistingServer: !process.env.CI
 	},
-	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testDir: 'e2e'
 });
